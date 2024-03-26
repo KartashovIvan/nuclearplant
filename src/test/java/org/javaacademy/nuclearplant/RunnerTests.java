0348518a1453692029c5ed9693aca59c8d6ac758
Kartashov Ivan <vano.noname69@yandex.ru>
2024-03-26 20:05:12 +0300

    init

---
+++ src/test/java/org/javaacademy/nuclearplant/RunnerTests.java
@@ -0,0 +1,13 @@
+package org.javaacademy.nuclearplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
